cb31432db6027c13c79655a021c75f40380d0fa2
AlejandroValderrama <avalte07gmail.com>
2023-02-02 14:25:04 +0100

    Correcciones

--- src/main/java/org/prueba/Main.java
+++ src/main/java/org/prueba/Main.java
@@ -8,30 +8,53 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String ruta = System.getenv("VIAFIRMA_PATH2");
 
-        addPage(System.getenv("VIAFIRMA_PATH"));
+        if(ruta!=null)
+            addPage(ruta);
+        else
+            System.err.println("La variable de entorno \"VIAFIRMA_PATH\" no existe.");
 
     }
 
-    public static String[] lista(String ruta) {
+    public static File[] lista(String ruta) {
         File archivos = new File(ruta); //Obtengo los archivos de la ruta fijada en la variable de entorno.
-        String[] listaPdf;
+        File[] pdfs;
 
-        listaPdf = archivos.list(new Filtro(".pdf")); // Filtro los archivos para que sean solo pdf.
+        if(archivos.isDirectory()) {
+            pdfs = archivos.listFiles(new Filtro(".pdf")); // Filtro los archivos para que sean solo pdf
+        } else {
+            if(archivos.isFile())
+                System.err.println("La variable de entorno \"VIAFIRMA_PATH\" apunta a un fichero.");
+            else
+                System.err.println("La variable de entorno \"VIAFIRMA_PATH\" apunta a un directorio que no existe.");
 
-        return listaPdf;
+            pdfs = null;
+        }
+
+        return pdfs;
     }
 
     public static void addPage(String ruta) throws IOException {
-        String[] listaPdf = lista(ruta); // Obtengo solo los pdfs.
+        File[] pdfs = lista(ruta); // Obtengo solo los pdfs.
 
-        for (String contenido: listaPdf) { // Recorro la lista
-            File pdf = new File(ruta+"\\"+contenido); // Genero un objeto file del pdf
-            PDDocument docu = PDDocument.load(pdf); // Cargo el documento
-            docu.addPage(new PDPage()); // Añado un nueva página al documento.
-            docu.save(pdf); // Guardo los cambios y cierro el documento.
-            docu.close();
+        if(pdfs != null) {
+
+            for (File contenido: pdfs) { // Recorro la lista
+                File pdf = new File(contenido.getPath()); // Genero un objeto file del pdf
+                PDDocument docu = PDDocument.load(pdf); // Cargo el documento
+
+                try {
+                    docu.addPage(new PDPage()); // Añado un nueva página al documento.
+                    docu.save(pdf); // Guardo los cambios y cierro el documento.
+                } finally {
+                    docu.close();
+                }
+
+            }
+
         }
+
 
     }
 
